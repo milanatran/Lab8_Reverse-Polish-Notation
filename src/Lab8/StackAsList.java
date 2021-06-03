0815aae5c00e4b664ede30ca0b848595483e8cd0
MILANA TRAN <MILANATRAN.NTM@gmail.com>
2021-06-03 19:41:41 +0200

    Corrected mistakes in the StackAsList methods

--- src/Lab8/StackAsList.java
+++ src/Lab8/StackAsList.java
@@ -1,5 +1,7 @@
 package Lab8;
 
+import java.util.Arrays;
+
 /**
  * Everytime a new element is pushed to the stack, it is not saved into the stack but in
  * a separate variable to access it easily when popping or peeking.
@@ -20,10 +22,11 @@ public class StackAsList<T> implements Stack<T> {
 		this.topElem = topElem;
 	}
 
-	@Override
+	//@Override
 	//Pushes an element to the top of the stack 
 	//A new stack is created with the element added to the old stack as well as the old stack. 
-	public void push(T topElem) {
+	/*public void push(T topElem) {
+		if(this.topElem == null) {}
 		//saving the current second element as new second element before adding it to oldStack
 		this.newSecond = this.secondElem;
 		//saving the second element into the old stack to make space for new element
@@ -31,6 +34,17 @@ public class StackAsList<T> implements Stack<T> {
 		
 		//saving the old top element as second element
 		this.secondElem = this.topElem;
+		//saving the new element as top element
+		this.topElem = topElem;
+	}*/
+	
+	@Override
+	//Pushes an element to the top of the stack 
+	//A new stack is created with the element added to the old stack as well as the old stack. 
+	public void push(T topElem) {
+		//adding the old top element into the old stack to make space for new element
+		this.oldStack = new StackAsList(this.oldStack, this.topElem);
+		
 		//saving the new element as top element
 		this.topElem = topElem;
 	}
@@ -41,13 +55,20 @@ public class StackAsList<T> implements Stack<T> {
 	 * @return The element on the top of the stack 
 	 */
 	public T pop() {
-		//saving the current top in a var
-		T top = this.topElem;
-		//making the second element the new top element
-		this.topElem = this.secondElem; 
-		//making the old second element new second element
-		this.secondElem = this.newSecond;
+		T top;
+		if(isEmpty() == true) {
+			top = null;
+		} else if (oldStack == null) {
+			top = this.topElem;
+			this.topElem = null;
+		} else {
+			//saving the current top in a var
+			top = this.topElem;
+			//making the second element the new top element
+			this.topElem = oldStack.pop();
+		}
 		return top;
+		
 	}
 	
 	@Override
@@ -60,17 +81,31 @@ public class StackAsList<T> implements Stack<T> {
 	@Override
 	//Checks whether the stack is empty or not
 	public boolean isEmpty() {
-		boolean empty = false;
+		boolean empty = true;
 		//when there is no top element, the stack is empty
-		if(topElem == null) empty = true;
+		if(topElem != null && oldStack != null) empty = false;
 		return empty;
 	}
 
 	
 	@Override
 	public String toString() {
-		
-		return "";
+		StackAsList<T> aStack = this.oldStack;
+		String s = "";
+		//only one element in the stack
+		//if(aStack == null && topElem != null) return "[" + topElem + "]";
+		//no element in the stack
+	    if(aStack == null) return "[]";
+		else {
+			while (aStack.isEmpty() == false) {
+				//remove the elements from the stack and add them to string s
+				T element = aStack.pop();
+				s += element.toString() + ", ";
+			}
+			String str = topElem + ", " + s;
+			return "[" + str.substring(0, str.length() -2) + "]";
+			//return "[" + topElem + ", " + s + "]";
+		}
 	}
 
 	
